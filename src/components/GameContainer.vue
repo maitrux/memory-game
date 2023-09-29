1f936018a432c2d49284c37da8a60b9724086892
Mai Trux <maitrux@hotmail.com>
2023-09-29 17:41:50 +0200

    started working with only words

--- src/components/GameContainer.vue
+++ src/components/GameContainer.vue
@@ -1,30 +1,30 @@
 <script>
+import CardStore from "@/store/CardStore.ts";
+import MemoryCard from "./MemoryCard.vue";
+
 export default {
+  components: {
+    MemoryCard,
+  },
+
   data() {
     return {
-      gridItems: [
-        { title: "Card 1", description: "Description for Card 1" },
-        { title: "Card 2", description: "Description for Card 2" },
-        { title: "Card 3", description: "Description for Card 3" },
-        { title: "Card 4", description: "Description for Card 4" },
-        { title: "Card 5", description: "Description for Card 5" },
-        { title: "Card 6", description: "Description for Card 6" },
-        { title: "Card 7", description: "Description for Card 7" },
-        { title: "Card 8", description: "Description for Card 8" },
-        { title: "Card 9", description: "Description for Card 9" },
-        { title: "Card 10", description: "Description for Card 10" },
-        { title: "Card 11", description: "Description for Card 11" },
-        { title: "Card 12", description: "Description for Card 12" },
-        { title: "Card 13", description: "Description for Card 13" },
-        { title: "Card 14", description: "Description for Card 14" },
-        { title: "Card 15", description: "Description for Card 15" },
-        { title: "Card 16", description: "Description for Card 16" },
-        { title: "Card 17", description: "Description for Card 17" },
-        { title: "Card 18", description: "Description for Card 18" },
-        { title: "Card 19", description: "Description for Card 19" },
-        { title: "Card 20", description: "Description for Card 20" },
-      ],
+      words: this.generateMappedWords(CardStore.wordPairs),
     };
+  },
+
+  methods: {
+    generateMappedWords(wordPairs) {
+      // shuffle the wordPairs and then take the first 10
+      const wordPairsToUse = wordPairs.slice(0, 10);
+
+      const words = wordPairsToUse.flatMap((pair) => [
+        pair.word,
+        pair.translation,
+      ]);
+
+      return words;
+    },
   },
 };
 </script>
@@ -33,25 +33,17 @@ export default {
   <v-container>
     <v-row>
       <v-col
-        v-for="(item, index) in gridItems"
+        v-for="(word, index) in words"
         :key="index"
         cols="12"
         sm="6"
-        md="4"
+        md="3"
         lg="3"
       >
-        <v-card class="memory-card">
-          <v-card-title>{{ item.title }}</v-card-title>
-          <v-card-text>{{ item.description }}</v-card-text>
-        </v-card>
+        <memory-card :word="word" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
-<style scoped lang="scss">
-.memory-card {
-  background-color: #55cdfd;
-  /* Set a fixed aspect ratio */
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/components/MemoryCard.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    word: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <v-card class="memory-card">
+    <v-card-title>{{ word }}</v-card-title>
+  </v-card>
+</template>
+
+<style scoped lang="scss">
+.memory-card {
+  background-color: #55cdfd;
+}
+</style>
